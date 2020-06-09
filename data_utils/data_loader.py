09edd85dd8d301d092077faade174ab5f640c3dc
Samir Akre <sakre@ucdavis.edu>
2020-06-09 15:14:25 -0700

    :sparkles: added hsc/mpp parent cell by library id to data loader

--- data_utils/data_loader.py
+++ data_utils/data_loader.py
@@ -1,5 +1,6 @@
 from .step7_to_long_format import step7_out_to_long_format
 import pandas as pd
+import numpy as np
 import json
 from pathlib import Path
 from tqdm import tqdm
@@ -7,17 +8,22 @@ from tqdm import tqdm
 
 class HSPC_data_loader():
     def __init__(self, config_path):
-        config = load_config(config_path)
-        print('Finding data files from:', config['step7'])
+        self.config = load_config(config_path)
+        print('Finding data files from:', self.config['step7'])
         self.data_files = list(
-            Path(config['step7']).glob(config['user'] + '*.txt')
+            Path(self.config['step7']).glob(self.config['user'] + '*.txt')
         )
         print('\tFound', len(self.data_files), 'files')
         self.validated: bool = False
 
+        print('Loading and transforming to long format')
         self.data: pd.DataFrame = self.load_format_data(self.data_files)
 
-        self.metadata: pd.DataFrame = self.load_metadata(config['metadata'])
+        print('Marking parent cells by library ID')
+        self.mark_lib_codes()
+
+        print('Validating data')
+        self.metadata: pd.DataFrame = self.load_metadata(self.config['metadata'])
         self.validated = self.validate()
 
     def validate(self):
@@ -35,13 +41,44 @@ class HSPC_data_loader():
             print('Validation/Data mismatch')
             print('Pre-validation shape:', self.data.shape, 'Post validation shape:', merged.shape)
             print(na)
+        else:
+            print('Data Validation Passed')
         self.data = merged
         return True
+
+    def mark_lib_codes(self):
+        lib_codes = pd.read_csv(
+            self.config['lib_codes'],
+            sep='\t',
+            header=None,
+            names=['lib_id', 'lib_code', 'lib_code_reverse']
+        )
+        lib_ids = pd.read_excel(
+            self.config['lib_ids'],
+            names=['mouse_id', 'condition', 'hsc_lib_id', 'mpp_lib_id']
+        )
+        for (m_id, hsc_id, mpp_id), _ in lib_ids.groupby(['mouse_id', 'hsc_lib_id', 'mpp_lib_id']):
+            hsc_code = lib_codes[lib_codes.lib_id == hsc_id].lib_code.tolist()[0]
+            mpp_code = lib_codes[lib_codes.lib_id == mpp_id].lib_code.tolist()[0]
+            hsc_query = (
+                self.data.code.str.startswith(hsc_code) &
+                (self.data.mouse_id == m_id)
+            )
+            self.data.loc[hsc_query, 'parent_cell_type'] = 'hsc'
+            mpp_query = (
+                self.data.code.str.startswith(mpp_code) &
+                (self.data.mouse_id == m_id)
+            )
+            self.data.loc[mpp_query, 'parent_cell_type'] = 'mpp'
+
+
+
 
     @staticmethod
     def load_format_data(data_files):
         formatted = []
-        for file_path in tqdm(data_files, desc='Loading and transforming to long format'):
+
+        for file_path in data_files:
             formatted.append(step7_out_to_long_format(file_path))
         return pd.concat(formatted)
     
